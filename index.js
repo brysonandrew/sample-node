67f231712f9e96384a466bc5c97a94a073df2394
Andrew Bryson <andrew@codebro.io>
2018-06-23 14:08:16 +0200

    Changed message

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hey, I\'m a Node.js app!')
+    res.send('Hey, I\'m a Node.js app! How are you')
 })
 
 app.listen(3000, () => {
-    console.log('Server is up on 3000')
+    console.log('Server is up on port 3000')
 })
